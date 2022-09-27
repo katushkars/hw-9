7188f02fa1d6bb0ff7cf9e1c3eb376c5f3b9beda
Екатерина <katushkars@mail.ru>
2022-09-27 17:11:06 +0500

    выполнено

---
+++ src/Author.java
@@ -0,0 +1,17 @@
+public class Author {
+    String name;
+    String firstName;
+    public Author (String name, String firstName) {
+        this.name=name;
+        this.firstName = firstName;
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+}
